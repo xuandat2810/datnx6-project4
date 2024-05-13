5afcbd6faaa65b84e3c40d52d4dd4259b9d47586
Dat <xuandat28105@gmail.com>
2024-05-13 10:40:05 +0700

    hello

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-2dohqnvuga4pvhdd.us.auth0.com',
+  clientId: 'LLz2PkHARG1eOfVCTYhIxuLzUZ5UGIea',
+  callbackUrl: 'http://localhost:3000/callback'
+};
